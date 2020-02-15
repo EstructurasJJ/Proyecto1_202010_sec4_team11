c67414be02898d1058dbf88348ad3ba234fb0122
jjbelran201819446 <jj.beltran@uniandes.edu.co>
2020-02-15 01:41:00 -0500

    master actualizada con la entrega de diseño al 60%

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,27 +2,26 @@ package controller;
 
 import java.util.Scanner;
 
+
+
+import model.data_structures.ListaEnlazadaQueue;
+import model.logic.Comparendo;
 import model.logic.Modelo;
 import view.View;
 
 public class Controller {
 
-	/* Instancia del Modelo*/
 	private Modelo modelo;
-	
-	/* Instancia de la Vista*/
 	private View view;
-	
-	/**
-	 * Crear la vista y el modelo del proyecto
-	 * @param capacidad tamaNo inicial del arreglo
-	 */
+	public final static String RUTAGEOJASON = "./data/comparendos_dei_2018.geojson";
+	public final static String JUEGUEMOS = "./data/comparendos_dei_2018_small.geojson";
+
 	public Controller ()
 	{
 		view = new View();
 		modelo = new Modelo();
 	}
-		
+
 	public void run() 
 	{
 		Scanner lector = new Scanner(System.in);
@@ -30,74 +29,40 @@ public class Controller {
 		String dato = "";
 		String respuesta = "";
 
-		while( !fin ){
+		while( !fin )
+		{
 			view.printMenu();
 
 			int option = lector.nextInt();
-			switch(option){
-				case 1:
-					view.printMessage("--------- \nCrear Arreglo \nDar capacidad inicial del arreglo: ");
-				    int capacidad = lector.nextInt();
-				    modelo = new Modelo(capacidad); 
-				    view.printMessage("Arreglo Dinamico creado");
-				    view.printMessage("Numero actual de elementos " + modelo.darTamano() + "\n---------");						
-					break;
+			switch(option)
+			{
+			case 1:
+				modelo.leerGeoJson(JUEGUEMOS);
+				view.printMessage("Archivo GeoJSon Cargado");
+				view.printMessage("Numero actual de comparendos " + modelo.darTamanio() + "\n---------");						
+				break;
 
-				case 2:
-					view.printMessage("--------- \nDar cadena (simple) a ingresar: ");
-					dato = lector.next();
-					modelo.agregar(dato);
-					view.printMessage("Dato agregado");
-					view.printMessage("Numero actual de elementos " + modelo.darTamano() + "\n---------");						
-					break;
+			case 2:
+				
+				
+				break;
+				
+			case 3:
+				
+				
+				break;
 
-				case 3:
-					view.printMessage("--------- \nDar cadena (simple) a buscar: ");
-					dato = lector.next();
-					respuesta = modelo.buscar(dato);
-					if ( respuesta != null)
-					{
-						view.printMessage("Dato encontrado: "+ respuesta);
-					}
-					else
-					{
-						view.printMessage("Dato NO encontrado");
-					}
-					view.printMessage("Numero actual de elementos " + modelo.darTamano() + "\n---------");						
-					break;
+			case 4:
+				view.printMessage("--------- \n Hasta pronto !! \n---------"); 
+				lector.close();
+				fin = true;
+				break;	
 
-				case 4:
-					view.printMessage("--------- \nDar cadena (simple) a eliminar: ");
-					dato = lector.next();
-					respuesta = modelo.eliminar(dato);
-					if ( respuesta != null)
-					{
-						view.printMessage("Dato eliminado "+ respuesta);
-					}
-					else
-					{
-						view.printMessage("Dato NO eliminado");							
-					}
-					view.printMessage("Numero actual de elementos " + modelo.darTamano() + "\n---------");						
-					break;
-
-				case 5: 
-					view.printMessage("--------- \nContenido del Arreglo: ");
-					view.printModelo(modelo);
-					view.printMessage("Numero actual de elementos " + modelo.darTamano() + "\n---------");						
-					break;	
-					
-				case 6: 
-					view.printMessage("--------- \n Hasta pronto !! \n---------"); 
-					lector.close();
-					fin = true;
-					break;	
-
-				default: 
-					view.printMessage("--------- \n Opcion Invalida !! \n---------");
-					break;
+			default: 
+				view.printMessage("--------- \n Opción Invalida !! \n---------");
+				break;
 			}
 		}
-		
+
 	}	
 }
