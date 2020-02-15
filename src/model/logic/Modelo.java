3b6f52d0492df9855aee1c017ebc82f18f7e19ec
jjbelran201819446 <jj.beltran@uniandes.edu.co>
2020-02-15 01:29:03 -0500

    Entrega al 60%

--- src/model/logic/Modelo.java
+++ src/model/logic/Modelo.java
@@ -1,72 +1,215 @@
 package model.logic;
 
-import model.data_structures.ArregloDinamico;
-import model.data_structures.IArregloDinamico;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
-/**
- * Definicion del modelo del mundo
- *
- */
-public class Modelo {
-	/**
-	 * Atributos del modelo del mundo
-	 */
-	private IArregloDinamico datos;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSyntaxException;
+
+import model.data_structures.ListaEnlazadaQueue;
+import model.data_structures.ListaEnlazadaSimple;
+import model.data_structures.Node;
+
+//TODO Ambos
+
+public class Modelo 
+{
+	private String parteDelComparendo; 
+	private Comparendo compaAgregar;
 	
-	/**
-	 * Constructor del modelo del mundo con capacidad predefinida
-	 */
+	//TODO Bobby
+	private ListaEnlazadaQueue<Comparendo> booty = new ListaEnlazadaQueue<Comparendo>();
+
 	public Modelo()
 	{
-		datos = new ArregloDinamico(7);
+		parteDelComparendo = "";
+		
+		//TODO Bobby
+		booty = new ListaEnlazadaQueue<Comparendo>();
 	}
 	
-	/**
-	 * Constructor del modelo del mundo con capacidad dada
-	 * @param tamano
-	 */
-	public Modelo(int capacidad)
+	
+	public ListaEnlazadaQueue<Comparendo> darDatos()
 	{
-		datos = new ArregloDinamico(capacidad);
+		return booty;
 	}
 	
-	/**
-	 * Servicio de consulta de numero de elementos presentes en el modelo 
-	 * @return numero de elementos presentes en el modelo
-	 */
-	public int darTamano()
+	//TODO Bobby
+	
+	public int darTamanio()
 	{
-		return datos.darTamano();
+		return booty.darTamanio();
 	}
-
-	/**
-	 * Requerimiento de agregar dato
-	 * @param dato
-	 */
-	public void agregar(String dato)
+	
+	//TODO Ambos
+	public void leerGeoJson(String pRuta) 
 	{	
-		datos.agregar(dato);
+        JsonParser parser = new JsonParser();
+        FileReader fr = null;
+        
+		try 
+		{
+			fr = new FileReader(pRuta);
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		
+        JsonElement datos = parser.parse(fr);
+        dumpJSONElement(datos);
+
 	}
 	
-	/**
-	 * Requerimiento buscar dato
-	 * @param dato Dato a buscar
-	 * @return dato encontrado
-	 */
-	public String buscar(String dato)
+	//TODO Ambos
+	private void dumpJSONElement(JsonElement elemento) 
 	{
-		return datos.buscar(dato);
+		
+		if (elemento.isJsonObject()) 
+		{
+			
+	        JsonObject obj = elemento.getAsJsonObject();
+	        
+	        java.util.Set<java.util.Map.Entry<String,JsonElement>> entradas = obj.entrySet();
+	        java.util.Iterator<java.util.Map.Entry<String,JsonElement>> iter = entradas.iterator();
+	        
+	        while (iter.hasNext()) 
+	        {
+	            java.util.Map.Entry<String,JsonElement> entrada = iter.next();
+	            componentesDelComparendo(entrada.getKey());	            
+
+	            dumpJSONElement(entrada.getValue());
+	        }
+
+	    }
+		else if (elemento.isJsonArray()) 
+		{			
+	        JsonArray array = elemento.getAsJsonArray();
+	        java.util.Iterator<JsonElement> iter = array.iterator();
+	        
+	        while (iter.hasNext()) 
+	        {
+	            JsonElement entrada = iter.next();
+	            dumpJSONElement(entrada);
+	        }
+
+	    } 
+		else if (elemento.isJsonPrimitive()) 
+		{
+	        JsonPrimitive valor = elemento.getAsJsonPrimitive();
+	        
+	        if(compaAgregar == null)
+	        {
+	    		compaAgregar = new Comparendo();
+	        }
+	        
+	        if(parteDelComparendo.equals("OBJECTID"))
+	        {
+	        	compaAgregar.asignarObjectid(valor.getAsInt());
+	        	//System.out.println(valor);
+	        }
+	        else if (parteDelComparendo.equals("FECHA_HORA"))
+			{
+	        	compaAgregar.asignarFecha_Hora(valor.getAsString());
+				//System.out.println(valor);
+			}
+			else if (parteDelComparendo.equals("MEDIO_DETE"))
+			{
+				compaAgregar.asignarMedio_Dete(valor.getAsString());
+				//System.out.println(valor);
+			}
+			else if (parteDelComparendo.equals("CLASE_VEHI"))
+			{
+				compaAgregar.asignarClase_Vehi(valor.getAsString());
+				//System.out.println(valor);
+			}
+			else if (parteDelComparendo.equals("TIPO_SERVI"))
+			{
+				compaAgregar.asignarTipo_Servicio(valor.getAsString());
+				//System.out.println(valor);
+			}
+			else if (parteDelComparendo.equals("INFRACCION"))
+			{
+				compaAgregar.asignarInfraccion(valor.getAsString());
+				//System.out.println(valor);
+			}
+			else if (parteDelComparendo.equals("DES_INFRAC"))
+			{
+				compaAgregar.asignarDes_Infrac(valor.getAsString());
+				//System.out.println(valor);
+				
+			}
+			else if (parteDelComparendo.equals("LOCALIDAD"))
+			{				
+				compaAgregar.asignarLocalidad(valor.getAsString());
+				
+				//System.out.println(valor);
+				//System.out.println("###______________###");
+				
+				booty.enqueue(compaAgregar);
+				
+				
+				parteDelComparendo = "";
+				compaAgregar = null;
+				
+				//System.out.println("////////////////////////////////////////AGREGADO////////////////////////////////////////");
+				
+			}
+	    } 
+		else if (elemento.isJsonNull()) 
+		{
+	        System.out.println("Es NULL");
+	    } 
+		else 
+		{
+	        System.out.println("Es otra cosa");
+	    }
+		
 	}
 	
-	/**
-	 * Requerimiento eliminar dato
-	 * @param dato Dato a eliminar
-	 * @return dato eliminado
-	 */
-	public String eliminar(String dato)
+	//TODO Ambos
+	public void componentesDelComparendo(String palabra)
 	{
-		return datos.eliminar(dato);
+		if (palabra.equals("OBJECTID"))
+		{
+			parteDelComparendo = "OBJECTID";
+		}
+		else if (palabra.equals("FECHA_HORA"))
+		{
+			parteDelComparendo = "FECHA_HORA";
+		}
+		else if (palabra.equals("MEDIO_DETE"))
+		{
+			parteDelComparendo = "MEDIO_DETE";
+		}
+		else if (palabra.equals("CLASE_VEHI"))
+		{
+			parteDelComparendo = "CLASE_VEHI";
+		}
+		else if (palabra.equals("TIPO_SERVI"))
+		{
+			parteDelComparendo = "TIPO_SERVI";
+		}
+		else if (palabra.equals("INFRACCION"))
+		{
+			parteDelComparendo = "INFRACCION";
+		}
+		else if (palabra.equals("DES_INFRAC"))
+		{
+			parteDelComparendo = "DES_INFRAC";
+		}
+		else if (palabra.equals("LOCALIDAD"))
+		{
+			parteDelComparendo = "LOCALIDAD";
+		}
 	}
+	
 
-
+	
+	
 }
